3511e43f322c9ff2834be8ab3b0bf55e30967f9b
ThanhTu930 <tuho3009@gmail.com>
2026-08-07 14:34:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def clean_currency(val):
 
 
 # --- 5. PHẦN TẢI FORM MẪU (Ở TRÊN) ---
-st.write("**TẢI FILE BÁO GIÁ MẪU ĐỂ NHẬP THEO FORM CỦA HỆ THỐNG**")
+st.write("TẢI FILE BÁO GIÁ MẪU ĐỂ NHẬP THEO FORM CỦA HỆ THỐNG")
 
 sample_file_data = generate_sample_template()
 st.download_button(
@@ -158,7 +158,7 @@ st.download_button(
 st.divider()
 
 # --- 6. PHẦN UPLOAD FILE (Ở DƯỚI) ---
-st.write("****UPLOAD FILE EXCEL TẠI ĐÂY (.xlsx, .xls):****")
+st.write("UPLOAD FILE EXCEL TẠI ĐÂY (.xlsx, .xls):")
 
 uploaded_file = st.file_uploader(
     "Upload", type=["xlsx", "xls"], label_visibility="collapsed"
