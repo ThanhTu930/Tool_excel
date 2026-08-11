c7dd8a43134e9d2ade9c1747eef5670f7fffc9b0
ThanhTu930 <tuho3009@gmail.com>
2026-08-11 10:47:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -720,8 +720,8 @@ if uploaded_file is not None:
             img = Image("logo_dvc.png")
             
             # Chỉnh kích thước logo (chiều rộng x chiều cao tính theo pixel)
-            img.width = 90
-            img.height = 90
+            img.width = 150
+            img.height = 150
             
             # Gán vị trí đặt logo tại ô A1
             ws_bg.add_image(img, "A2")
